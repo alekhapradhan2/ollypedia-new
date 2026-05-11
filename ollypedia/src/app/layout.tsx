162c63d65b0e78d8acc70467eff4ffc0f643ae07
Subhasree <subhasree1289@gmail.com>
2026-05-11 15:59:12 +0530

    Added Google AdSense script

--- ollypedia/src/app/layout.tsx
+++ ollypedia/src/app/layout.tsx
@@ -31,11 +31,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="dark">
           <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXX"
-          crossOrigin="anonymous"
-        ></script>
+  <script
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5823659147566885"
+  crossOrigin="anonymous"
+></script>
       </head>
       <body className="grain min-h-screen flex flex-col bg-[#0a0a0a]">
         <Navbar />
